36217bada832a38a2b330bcc261284bdf9e8214d
Daniel <dani3lkamanthi@gmail.com>
2024-03-14 19:10:11 +0300

    Fixes errors

--- errors.js
+++ errors.js
@@ -1,8 +1,6 @@
-// Fix the error in the snackSelection code snippet first
-
 const snackSelection = 'Pretzels';
 
-if (snackSelection = 'Pretzels') {
+if (snackSelection == 'Pretzels') {
   console.log("That'll be $1, please!");
 }
 
@@ -15,6 +13,10 @@ function first () {
 
 function second () {
   third();
+}
+
+function third() {
+  console.log("Now I'm a function!");
 }
 
 first();
